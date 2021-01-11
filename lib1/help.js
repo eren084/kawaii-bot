f635494cd2ab2178d017b79a89b5d1fe520620d5
eren084 <77036756+eren084@users.noreply.github.com>
2021-01-11 10:40:12 -0300

    Bot kawaai

---
+++ lib1/help.js
@@ -0,0 +1,168 @@
+const help = (prefix) => {
+	return `♥ kawaii bot ♥  
+*Principais Comandos* 
+
+Comando: '${prefix}sticker' ou '${prefix}stiker'
+Função: converter imagem / gif / vídeo em adesivo
+Como Usar: responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda 
+
+Comando: '${prefix}sticker nobg' ou '${prefix}stiker nobg'
+Função: converter imagem em adesivo removendo o fundo
+Como Usar: responder a imagem, ou enviar com legenda
+
+Comando: '${prefix}toimg'
+Função: converter adesivos em imagem
+Como Usar: responder a sticker 
+
+Comando:'${prefix}criador'
+Função: Mostra as informações do criador do bot
+
+*Comandos de Memes* 
+
+Comando: '${prefix}meme'
+Função: imagens aleatórias de meme [english]
+
+Comando: '${prefix}memeindo'
+Função: imagens aleatórias de meme [indo]
+
+
+*Comandos de texto* 
+
+Comando: '${prefix}frase'
+Função: Envia uma frase filosofica
+
+Comando: '${prefix}textph'
+Função: converte o texto na logo da pornhub
+Como Usar: '${prefix}textph [pt1] | [pt2]'
+
+Comando: '${prefix}traduz'
+Função: traduz seu texto em outra lingua
+Como Usar: '${prefix}traduz [la] [texto]'
+Exemplo: '${prefix}traduz pt hello my friends'
+
+Comando: '${prefix}idioma'
+Função: lista todos os códigos de idioma
+
+Comando: '${prefix}text3d'
+Função: converte o texto em imagem 3d
+Como Usar: '${prefix}text3d [pt1]'
+
+Comando: '${prefix}wolflogo'
+Função: converte o texto em imagem com o logo de um lobo
+Como Usar: '${prefix}wolflogo [pt1] | [pt2]'
+
+Comando: '${prefix}lionlogo'
+Função: converte o texto em imagem com o logo de um leao
+Como Usar: '${prefix}lionlogo [pt1] | [pt2]'
+
+Comando: '${prefix}glitch'
+Função: converte o texto em imagem com estilo glitch
+Como Usar: '${prefix}glitch [pt1] | [pt2]'
+
+Comando:'${prefix}ninjalogo'
+Função: converte o texto em imagem com o logo de um ninja
+Como Usar:'${prefix}ninjalogo [pt1] | [pt2]'
+
+Comando: '${prefix}neontec'
+Função: converte o texto em imagem com estilo neon tecnology
+Como Usar: '${prefix}neontec [pt1]'
+
+Comando:'${prefix}neonligth'
+Função: converte o texto em imagem com estilo neon ligth
+Como Usar:'${prefix}neonligth [pt1]'
+
+Comando:'${prefix}snow'
+Função: converte o texto em imagem com estilo em snow'
+Como Usar:'${prefix}snow [pt1]'
+
+Comando:'${prefix}blood'
+Função: converte o texto em imagem com estilo blood
+Como Usar:'${prefix}blood [pt1]'
+
+Comando:'${prefix}dropwater'
+Função: converte o texto em imagem com estilo dropwater
+Como Usar:'${prefix}dropwater [pt1]'
+
+*Outros Comandos* 
+
+
+Comando: '${prefix}gtts'
+Função: converter texto em fala / áudio
+Como Usar: '${prefix}gtts [la] [texto]'
+Exemplo: '${prefix}gtts ja Oni-chan'
+
+Comando: '${prefix}signome'
+Função: Mostra o significado do seu nome
+Como usar:'${prefix}signome [nome]'
+Exemplo:'${prefix}signome ian'
+
+Comando: '${prefix}nethunter'
+Função: Mostra um tutorial de como ter um emulador de kali linux no seu termux
+
+Comando: '${prefix}db'
+Função: envia links de várias database de travazap
+
+Comando:'${prefix}wppim'
+Função: envia links de todos whatsapp imunes
+
+Comando: '${prefix}url2img'
+Função: tirar uma captura de tela na web
+Como Usar: *${prefix}url2img [tip] [url]*
+
+Comando: '${prefix}simi'
+Função: sua mensagem será respondida por simi
+Como Usar: '${prefix}simi [texto]'
+
+Comando: '${prefix}ocr'
+Função: pegue o texto na foto
+Como Usar: responder imagem ou enviar imagem com legenda 
+
+Comando: '${prefix}wait'
+Função: Pesquisa o anime na imagem [ O que anime é isso / aquilo ]
+Como Usar: responder imagem ou enviar imagem com legenda 
+
+Comando: '${prefix}setprefix'
+Função: substituir prefixo
+Como Usar: '${prefix}setprefix [text|optional]' 
+Exemplo: '${prefix}setprefix ?'
+
+ *Comandos de Grupos* 
+
+Comandos: '${prefix}termux'
+Função: Mostra uma lista de comandos básicos do termux
+
+Comandos: '${prefix}welcome'
+Função: Ativa o modo boas-vindas do grupo
+Como Usar: '${prefix}welcome 1' para ativar o modo welcome e '${prefix}welcome 0' para desativar o modo welcome
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comandos: '${prefix}linkgroup'
+Função: pegue o link do grupo
+Como Usar: just send the command
+Obs: só pode ser usado quando o bot se torna admin, e quem enviar o comando é admin! 
+
+Comandos: '${prefix}tagall'
+Função: marca todos os membros do grupo, incluindo administradores também
+Como Usar: apenas envie o comando
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comandos: '${prefix}simih'
+Função: ative o modo simi no grupo
+Como Usar: '${prefix}simih 1' para ativar o modo simih e '${prefix}simih 0' para desativar o modo simih
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+*Como instalar o bot??* 
+
+https://fir3.net/ELR5uKC
+
+🔰 Apoie o criador do bot a adicionar comandos novos 🥺 🔰
+
+https://enrt.eu/kN7iGtl4 (so de entrar no link ajuda)
+
+`
+}
+
+exports.help = help
+
+
+
